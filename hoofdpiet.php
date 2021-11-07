0844ddc1681e7c234c719f1c7f35b280e3910028
PeterVenhuizen <peter_venhuizen@hotmail.com>
2021-11-07 13:07:09 +0100

    Add basic styling and structure

--- hoofdpiet.php
+++ hoofdpiet.php
@@ -31,7 +31,47 @@
 
     <div id="app">
         <nav><?php include 'components/nav.php'; ?></nav>
-        <main></main>
+        <main>
+
+            <div id="surprise-planner">
+                <h2>Surprise planner</h2>
+                <div class="surprise">
+                    <time datetime="2019-12-05">05-12-2019</time>
+                    <span>
+                        &euro;<span class="prijsGroot">10,00</span>
+                    </span>
+                    <span>
+                        &euro;<span class="prijsKlein">25,00</span>
+                    </span>
+                    <i class="far fa-calendar"></i>
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class="surprise">
+                    <time datetime="2020-12-05">05-12-2020</time>
+                    <span>
+                        &euro;<span class="prijsGroot">10,00</span>
+                    </span>
+                    <span>
+                        &euro;<span class="prijsKlein">25,00</span>
+                    </span>
+                    <i class="far fa-calendar"></i>
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class="surprise actief">
+                    <time datetime="2022-01-01">01-01-2022</time>
+                    <span>
+                        &euro;<span class="prijsGroot">12,50</span>
+                    </span>
+                    <span>
+                        &euro;<span class="prijsKlein">25,00</span>
+                    </span>
+                    <i class="far fa-calendar-check"></i>
+                    <i class="fas fa-chevron-down"></i>
+                </div>
+                <button class="btn-primary">Plan nieuwe surprise</button>
+            </div>
+
+        </main>
     </div>
 
     <div id="modal-wrapper">
