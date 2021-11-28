a35f287c7880e8016badb241a548857db3a011e6
PeterVenhuizen <peter_venhuizen@hotmail.com>
2021-11-28 15:56:56 +0100

    Add favicon

--- index.php
+++ index.php
@@ -10,9 +10,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <base href="/sinterklaas/">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/pakje.ico" >
     <title>Sinterklaas</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
